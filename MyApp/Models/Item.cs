29a58db09a3c9fa3437c4c7aecf6639ae60fd550
Alehub369 <lievanoalejandro@gmail.com>
2025-07-09 21:52:14 -0500

    Editar con select un item

--- MyApp/Models/Item.cs
+++ MyApp/Models/Item.cs
@@ -9,6 +9,12 @@ namespace MyApp.Models
         public double Price { get; set; }
         public int? SerialNumberId { get; set; }
         public SerialNumber? SerialNumber { get; set; }
+
+        public int? CategoryId { get; set; }
+        [ForeignKey("CategoryId")]
+        public Category? Category { get; set; }
+
+
     }
 
 }
